f5452de82249befd0797bc284fa8095680be30d7
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-22 19:42:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,28 +26,9 @@ class Song
     def self.artists 
       @@artists.uniq
     end
-=begin
- 
-  def self.genre_count
-    sum = 0
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre] == nil
-        genre_count[genre] = 1;
-      else
-        genre_count[genre] += 1;
-      end
-    end
-    genre_count
-  end
-end
-=end
-#it "is a class method that returns a hash of genres and the number of songs that have those genres" do 
-    #  expect(Song.genre_count).to eq({"pop" => 1, "rap" => 2})
-    
     def self.genre_count
      # @@genres ={genre}
-    
+
       genre_count = {}
       @@genres.each do |genre|
         if genre_count[genre] != nil
@@ -58,10 +39,7 @@ end
     end
      genre_count
  end
- 
   def self.artist_count
-   
-    
       artist_count = {}
       @@artists.each do |artist|
         if artist_count[artist] != nil
